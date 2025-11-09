91aa17506dc25749567c6761d82434d5b31e78b2
Luminight <Luminight@luminight.dev>
2025-11-09 23:45:20 +0100

    setup recompiler flow

--- .nvimrc.lua
+++ .nvimrc.lua
@@ -1,3 +1,4 @@
 vim.keymap.set("n", "<F5>", "<cmd>!ib && ./main test test<CR>", { desc = "Run the app" })
 
+vim.keymap.set("n", "<leader>br", "<cmd>!(cd ../iblang/ && ./install.sh)<CR>", { desc = "Rebuild Compiler" })
 vim.keymap.set("n", "<leader>bb", "<cmd>!ib<CR>", { desc = "Build" })
