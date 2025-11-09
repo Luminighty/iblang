e6ca58ec16cf13cad45ea9fa061690af2f9da626
Luminight <Luminight@luminight.dev>
2025-11-09 23:34:43 +0100

    adjust stdlib

--- .nvimrc.lua
+++ .nvimrc.lua
@@ -1,2 +1,3 @@
 vim.keymap.set("n", "<F5>", "<cmd>!cargo run -- -e<CR>", { desc = "Run the app" })
+vim.keymap.set("n", "<F6>", "<cmd>!./install.sh<CR>", { desc = "Run the app" })
 vim.keymap.set("n", "<leader>bb", "<cmd>!cargo build<CR>", { desc = "Run the app" })
