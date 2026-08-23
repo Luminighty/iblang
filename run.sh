619d30e65ac7f82dace4758d1aa847dfb4c66862
Luminight <Luminight@luminight.dev>
2026-08-23 22:05:39 +0200

    start reporting typecheck errors, typecheck idents, typeidents

--- run.sh
+++ run.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-ib --entry=./src/main.ib --out=./bin/ib && echo "Running" && ./bin/ib --debug-ast
+
+# ib --entry=./src/main.ib --out=./bin/ib && echo "Running" && ./bin/ib --debug-ast
+ib --entry=./src/main.ib --out=./bin/ib && echo "Running" && ./bin/ib
