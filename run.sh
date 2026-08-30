2bf9aa1e257dafe669890ea2f6f85048a5da21a2
Luminight <Luminight@luminight.dev>
2026-08-30 14:04:16 +0200

    ast declaration, typecheck everything, start typecheck tests

--- run.sh
+++ run.sh
@@ -2,4 +2,9 @@
 
 
 # ib --entry=./src/main.ib --out=./bin/ib && echo "Running" && ./bin/ib --debug-ast
-ib --entry=./src/main.ib --out=./bin/ib && echo "===== Running =====" && ./bin/ib
+
+
+ib --entry=./src/main.ib --out=./bin/ib && \
+echo "===== Running =====" && \
+./bin/ib -dt
+
